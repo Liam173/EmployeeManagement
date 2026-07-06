d5638984a1a5bb38e15422917ad470b2277ffe6f
Liam173 <liamdupreez17@gmail.com>
2026-07-06 13:20:02 +0200

    DEV-00001 | EmployeeManagement API | Setup API system

---
+++ EmployeeManagement/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,13 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        List<Employee> GetAll();
+
+        Employee? GetById(int id);
+
+        void Add(Employee employee);
+    }
+}
---
+++ EmployeeManagement/Repositories/EmployeeRepository.cs
@@ -0,0 +1,42 @@
+﻿using EmployeeManagement.Interfaces;
+using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        private readonly List<Employee> _employees =
+        [
+            new Employee
+            {
+                Id = 1,
+                Name = "John",
+                Age = 30,
+                Salary = 40000
+            },
+
+            new Employee
+            {
+                Id = 2,
+                Name = "Alice",
+                Age = 27,
+                Salary = 50000
+            }
+        ];
+
+        public List<Employee> GetAll()
+        {
+            return _employees;
+        }
+
+        public Employee? GetById(int id)
+        {
+            return _employees.FirstOrDefault(e => e.Id == id);
+        }
+
+        public void Add(Employee employee)
+        {
+            _employees.Add(employee);
+        }
+    }
+}

cb1c499b59b957cdeee67812430195d23eb3805d
Liam173 <liamdupreez17@gmail.com>
2026-07-14 12:52:40 +0200

    DEV-00014 | Employee Management API | Background Services and Hosted Services

---
+++ EmployeeManagement/BackgroundServices/EmailBackgroundService.cs
@@ -0,0 +1,50 @@
+﻿using EmployeeManagement.Services;
+
+namespace EmployeeManagement.BackgroundServices
+{
+    public class EmailBackgroundService : BackgroundService
+    {
+        private readonly ILogger<EmailBackgroundService> _logger;
+
+        private readonly FakeEmailQueueService _queue;
+
+        public EmailBackgroundService(
+            ILogger<EmailBackgroundService> logger,
+            FakeEmailQueueService queue)
+        {
+            _logger = logger;
+            _queue = queue;
+        }
+
+        protected override async Task ExecuteAsync(
+            CancellationToken stoppingToken)
+        {
+            _logger.LogInformation(
+                "Email Background Service started.");
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var email = _queue.GetNextEmail();
+
+                if (email == null)
+                {
+                    _logger.LogInformation(
+                        "No emails waiting.");
+                }
+                else
+                {
+                    _logger.LogInformation(
+                        "Sending email to {Email}.",
+                        email);
+                }
+
+                await Task.Delay(
+                    TimeSpan.FromSeconds(15),
+                    stoppingToken);
+            }
+
+            _logger.LogInformation(
+                "Email Background Service stopped.");
+        }
+    }
+}
---
+++ EmployeeManagement/Services/FakeEmailQueueService.cs
@@ -0,0 +1,22 @@
+﻿namespace EmployeeManagement.Services
+{
+    public class FakeEmailQueueService
+    {
+        private readonly Queue<string> _emails = new();
+
+        public FakeEmailQueueService()
+        {
+            _emails.Enqueue("alice@example.com");
+            _emails.Enqueue("bob@example.com");
+            _emails.Enqueue("charlie@example.com");
+        }
+
+        public string? GetNextEmail()
+        {
+            if (_emails.Count == 0)
+                return null;
+
+            return _emails.Dequeue();
+        }
+    }
+}
